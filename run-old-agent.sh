ecfd0d4d9e5b74e1ab99e562bfe9198333aa2b21
taeyeon-kimm <taeyeon.kimm@navercorp.com>
2023-03-03 16:59:26 +0900

    Update old agent guide

--- run-old-agent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./gradlew build -p scavenger-old-agent-java -x test
-./gradlew build -p scavenger-demo -x test
-
-java -Dscavenger.configuration=./scavenger-demo/scavenger.conf  \
-     -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 \
-     -javaagent:$(find scavenger-old-agent-java/build/libs/scavenger-old-agent-java-*.jar | tail -1) \
-     -jar $(find scavenger-demo/build/libs/scavenger-demo-*.jar | tail -1)
