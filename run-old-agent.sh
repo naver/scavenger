751ff7a103598b29415f5bed9c4ff40c91919184
sohyun <sohyun.ku@nts-corp.com>
2023-03-03 11:54:45 +0900

    Modify JVM options order at run-agent.sh

--- run-old-agent.sh
+++ run-old-agent.sh
@@ -4,6 +4,6 @@
 ./gradlew build -p scavenger-demo -x test
 
 java -Dscavenger.configuration=./scavenger-demo/scavenger.conf  \
-     -javaagent:$(find scavenger-old-agent-java/build/libs/scavenger-old-agent-java-*.jar | tail -1) \
      -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 \
+     -javaagent:$(find scavenger-old-agent-java/build/libs/scavenger-old-agent-java-*.jar | tail -1) \
      -jar $(find scavenger-demo/build/libs/scavenger-demo-*.jar | tail -1)
