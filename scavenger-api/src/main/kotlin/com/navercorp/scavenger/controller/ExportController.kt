1e29eb10d8b98655e67286ae7dd8ebbc96889377
cheesecat47 <cheesecat47@gmail.com>
2023-04-18 01:49:38 +0900

    Update to use kotlin-csv
    Related PR #61
    
    Replace the csv library from apache commons-csv to kotlin-csv.
    
    References:
    - <https://kenta-koyama-biz.gitbook.io/kotlin-csv/#csv-write-examples>
    - <https://codeburst.io/returning-csv-content-from-an-api-in-spring-boot-63ea82bbcf0f>

--- scavenger-api/src/main/kotlin/com/navercorp/scavenger/controller/ExportController.kt
+++ scavenger-api/src/main/kotlin/com/navercorp/scavenger/controller/ExportController.kt
@@ -1,18 +1,22 @@
 package com.navercorp.scavenger.controller
 
 import com.navercorp.scavenger.service.ExportSnapshotMethodService
+import org.springframework.core.io.InputStreamResource
+import org.springframework.core.io.Resource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-
-import javax.servlet.http.HttpServletResponse
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 
 @RestController
 @RequestMapping("/api")
 class ExportController(
-    private val response: HttpServletResponse,
     private val exportSnapshotMethodService: ExportSnapshotMethodService
 ) {
     @GetMapping("/customers/{customerId}/export/snapshot/{snapshotId}", produces = ["text/csv"])
@@ -20,14 +24,20 @@ class ExportController(
         @PathVariable customerId: Long,
         @PathVariable snapshotId: Long,
         @RequestParam fn: String
-    ) {
-        response.characterEncoding = "UTF-8"
-        response.contentType = "text/csv; charset=UTF-8"
+    ): ResponseEntity<Resource> {
+        val byteArrayInputStream = ByteArrayOutputStream().use {
+            exportSnapshotMethodService.writeDtoToTsv(it, customerId, snapshotId)
+            ByteArrayInputStream(it.toByteArray())
+        }
 
-        response.setHeader(
-            "Content-disposition",
-            "attachment;filename=$fn"
+        val headers = HttpHeaders()
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=$fn")
+        headers.set(HttpHeaders.CONTENT_TYPE, "text/csv; charset=UTF-8")
+
+        return ResponseEntity(
+            InputStreamResource(byteArrayInputStream),
+            headers,
+            HttpStatus.OK
         )
-        exportSnapshotMethodService.writeDtoToTsv(response.writer, customerId, snapshotId)
     }
 }
--- scavenger-api/src/main/kotlin/com/navercorp/scavenger/dto/ExportSnapshotMethodDto.kt
+++ scavenger-api/src/main/kotlin/com/navercorp/scavenger/dto/ExportSnapshotMethodDto.kt
@@ -3,30 +3,30 @@ package com.navercorp.scavenger.dto
 import com.navercorp.scavenger.entity.ExportSnapshotMethodEntity
 
 data class ExportSnapshotMethodDto(
-    val filterInvokedAtMillis: Long,
-    val packages: String?,
-    val status: String?,
-    val excludeAbstract: String?,
-    val parent: String?,
+    val filterInvokedAtMillis: String,
+    val packages: String,
+    val status: String,
+    val excludeAbstract: String,
+    val parent: String,
     val signature: String,
-    val type: String?,
-    val usedCount: Int,
-    val unusedCount: Int,
-    val lastInvokedAtMillis: Long?
+    val type: String,
+    val usedCount: String,
+    val unusedCount: String,
+    val lastInvokedAtMillis: String
 ) {
     companion object {
         fun from(entity: ExportSnapshotMethodEntity): ExportSnapshotMethodDto {
             return ExportSnapshotMethodDto(
-                entity.filterInvokedAtMillis,
-                entity.packages,
-                entity.status,
-                entity.excludeAbstract,
-                entity.parent,
+                entity.filterInvokedAtMillis.toString(),
+                entity.packages ?: "",
+                entity.status ?: "",
+                entity.excludeAbstract ?: "",
+                entity.parent ?: "",
                 entity.signature,
-                entity.type,
-                entity.usedCount,
-                entity.unusedCount,
-                entity.lastInvokedAtMillis
+                entity.type ?: "",
+                entity.usedCount.toString(),
+                entity.unusedCount.toString(),
+                entity.lastInvokedAtMillis.toString()
             )
         }
     }
