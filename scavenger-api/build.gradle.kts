33b537d39abb8a109390a62a833130351f1d7188
sohyun-ku <sohyun.ku@nts-corp.com>
2023-05-10 11:52:35 +0900

    Remove kotlin-csv

--- scavenger-api/build.gradle.kts
+++ scavenger-api/build.gradle.kts
@@ -43,7 +43,6 @@ dependencies {
     implementation("io.github.microutils:kotlin-logging-jvm:2.1.23")
     implementation("net.ttddyy:datasource-proxy:1.7")
     implementation("com.github.vertical-blank:sql-formatter:1.0.1")
-    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.9.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
