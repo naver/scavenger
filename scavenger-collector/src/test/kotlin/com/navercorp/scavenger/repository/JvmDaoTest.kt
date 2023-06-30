5ab9f764f6160fdf1c7b2feceab5f7d71c41d948
1004647 <shsim@sk.com>
2023-06-30 14:40:12 +0900

    Update JvmDaoTest.kt
    remove unused import

--- scavenger-collector/src/test/kotlin/com/navercorp/scavenger/repository/JvmDaoTest.kt
+++ scavenger-collector/src/test/kotlin/com/navercorp/scavenger/repository/JvmDaoTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.transaction.annotation.Transactional
 import java.time.Instant
 
