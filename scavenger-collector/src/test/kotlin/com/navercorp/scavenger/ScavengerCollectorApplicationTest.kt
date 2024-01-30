c4a677b550e9e53f602c95147a5cdc8ea7c24d36
taeyeon-kimm <taeyeon.kimm@navercorp.com>
2024-01-31 00:27:38 +0900

    Fix schema-1.1.2.sql

---
+++ scavenger-collector/src/test/kotlin/com/navercorp/scavenger/ScavengerCollectorApplicationTest.kt
@@ -0,0 +1,11 @@
+package com.navercorp.scavenger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScavengerCollectorApplicationTest {
+    @Test
+    fun contextLoads() {
+    }
+}
