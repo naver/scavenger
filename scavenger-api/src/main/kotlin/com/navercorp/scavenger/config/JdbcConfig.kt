1e99be0bd6ababad205c5a4f2961cfca9f1cfbd0
sohyun-ku <sohyun.ku@nts-corp.com>
2024-06-03 16:16:14 +0900

    Polish

--- scavenger-api/src/main/kotlin/com/navercorp/scavenger/config/JdbcConfig.kt
+++ scavenger-api/src/main/kotlin/com/navercorp/scavenger/config/JdbcConfig.kt
@@ -22,7 +22,6 @@ import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration
 import org.springframework.data.relational.core.mapping.NamingStrategy
 import org.springframework.data.relational.core.mapping.RelationalMappingContext
 import org.springframework.data.relational.core.mapping.RelationalPersistentProperty
-import java.util.Collections
 import javax.sql.DataSource
 
 @Configuration
@@ -38,7 +37,7 @@ class JdbcConfig : AbstractJdbcConfiguration() {
         jdbcConverter: JdbcConverter
     ): SqlParameterSourceFactory {
         return EntityConvertibleSqlParameterSourceFactory(
-            ConvertibleParameterSourceFactory(DefaultJdbcParameterSourceConverter(Collections.emptyList()), NoneFallbackParameterSource()),
+            ConvertibleParameterSourceFactory(DefaultJdbcParameterSourceConverter(emptyList()), NoneFallbackParameterSource()),
             mappingContext,
             jdbcConverter
         )
