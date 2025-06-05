cf1d43d34ddd05985fcb294a1072ebd5b4558def
sohyun-ku <sohyun.ku@nts-corp.com>
2025-06-05 10:55:21 +0900

    Add vitess configuation

---
+++ scavenger-schema/vitess/sequence_init_data-1.1.4.sql
@@ -0,0 +1,1 @@
+INSERT INTO call_stacks_sequence (id, next_id, cache) VALUES (0, 0, 1000);
---
+++ scavenger-schema/vitess/sequence_schema-1.1.4.sql
@@ -0,0 +1,1 @@
+CREATE TABLE call_stacks_sequence (id BIGINT, next_id BIGINT, cache BIGINT, PRIMARY KEY (id)) comment 'vitess_sequence';
